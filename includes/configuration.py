0128b3c001ecd6f8bd790a5496708bd20fbb2ea4
jjcc0410 <jjcc0410>
2024-09-10 01:12:30 +0000

    create a variable for mount path

---
+++ includes/configuration.py
@@ -0,0 +1,4 @@
+# Databricks notebook source
+raw_folder_path = '/mnt/formula1dljjcc/raw'
+processed_folder_path = '/mnt/formula1dljjcc/processed'
+presentation_folder_path = '/mnt/formula1dljjcc/presentation'

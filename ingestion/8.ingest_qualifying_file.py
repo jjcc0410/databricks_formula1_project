353ecb66bb0162b6798555f0034fc1b264490ae6
jjcc0410 <jjcc0410>
2024-09-10 01:01:19 +0000

    ingest qualifying file

---
+++ ingestion/8.ingest_qualifying_file.py
@@ -0,0 +1,94 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC #Ingest qualifying folder
+
+# COMMAND ----------
+
+dbutils.widgets.text('p_data_source', '')
+v_data_source = dbutils.widgets.get('p_data_source')
+
+# COMMAND ----------
+
+dbutils.widgets.text('p_file_date', '2021-03-28')
+v_file_date = dbutils.widgets.get('p_file_date')
+
+# COMMAND ----------
+
+# MAGIC %run "../includes/configuration"
+
+# COMMAND ----------
+
+# MAGIC %run "../includes/common_functions"
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Step 1 - Read the JSON file using the spark dataframe reader API
+
+# COMMAND ----------
+
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType
+
+# COMMAND ----------
+
+qualifying_schema = StructType(
+    fields=[
+        StructField('qualifyId', IntegerType(), False),
+        StructField('raceId', IntegerType(), True),
+        StructField('driverId', IntegerType(), True),
+        StructField('constructorId', IntegerType(), True),
+        StructField('number', IntegerType(), True),
+        StructField('position', IntegerType(), True),
+        StructField('q1', StringType(), True),
+        StructField('q2', StringType(), True),
+        StructField('q3', StringType(), True),
+    ]
+)
+
+# COMMAND ----------
+
+qualifying_df = (
+    spark.read.schema(qualifying_schema)
+    .option('multiLine', True)
+    .json(f'{raw_folder_path}/{v_file_date}/qualifying')
+)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Step 2 - Rename columns and add new columns
+# MAGIC 1. Rename driverId and raceId
+# MAGIC 2. Add ingestion_Date with current timestamp
+
+# COMMAND ----------
+
+from pyspark.sql.functions import current_timestamp, lit
+
+# COMMAND ----------
+
+final_df = (
+    qualifying_df.withColumnRenamed("qualifyId", "qualify_id")
+    .withColumnRenamed("driverId", "driver_id")
+    .withColumnRenamed("raceId", "race_id")
+    .withColumnRenamed("constructorId", "constructor_id")
+    .withColumn("ingestion_date", current_timestamp())
+    .withColumn("data_source", lit(v_data_source))
+)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Step 3 - Write to output to processed container in parquet format
+
+# COMMAND ----------
+
+overwrite_partition(final_df, "f1_processed", "qualifying", "race_id")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT * FROM f1_processed.qualifying
+
+# COMMAND ----------
+
+dbutils.notebook.exit('Success')

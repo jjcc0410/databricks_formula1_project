a909b064678d77849389b5f0bd408ae339c31d52
jjcc0410 <jjcc0410>
2024-09-13 05:56:18 +0000

    fixed bug

--- trans/analysis/4.viz_dominant_teams.sql
+++ trans/analysis/4.viz_dominant_teams.sql
@@ -30,21 +30,19 @@ SELECT * FROM v_dominant_teams
 
 -- COMMAND ----------
 
--- MAGIC %python
--- MAGIC %sql
--- MAGIC SELECT 
--- MAGIC   CAST(CONCAT(CAST(race_year AS STRING), '-01-01') AS DATE) AS race_year_date,
--- MAGIC   team_name,
--- MAGIC   COUNT(1) AS total_races,
--- MAGIC   SUM(calculated_points) AS total_points,
--- MAGIC   AVG(calculated_points) AS avg_points
--- MAGIC FROM
--- MAGIC   f1_presentation.calculated_race_results
--- MAGIC WHERE team_name IN (SELECT team_name FROM v_dominant_teams WHERE team_rank <= 5)
--- MAGIC GROUP BY
--- MAGIC   race_year, team_name
--- MAGIC ORDER BY
--- MAGIC   race_year, avg_points DESC
+SELECT 
+  CAST(CONCAT(CAST(race_year AS STRING), '-01-01') AS DATE) AS race_year_date,
+  team_name,
+  COUNT(1) AS total_races,
+  SUM(calculated_points) AS total_points,
+  AVG(calculated_points) AS avg_points
+FROM
+  f1_presentation.calculated_race_results
+WHERE team_name IN (SELECT team_name FROM v_dominant_teams WHERE team_rank <= 5)
+GROUP BY
+  race_year, team_name
+ORDER BY
+  race_year, avg_points DESC
 
 -- COMMAND ----------
 

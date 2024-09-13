c593361e6b4802033d4be7db7f79ad0cce7ec9a1
jjcc0410 <jjcc0410>
2024-09-13 03:48:12 +0000

    ingestion now uses upsert method

--- ingestion/7.ingest_lap_times_file.py
+++ ingestion/7.ingest_lap_times_file.py
@@ -76,7 +76,12 @@ final_df = (
 
 # COMMAND ----------
 
-overwrite_partition(final_df, "f1_processed", "lap_times", "race_id")
+# overwrite_partition(final_df, "f1_processed", "lap_times", "race_id")
+
+# COMMAND ----------
+
+merge_condition = "tgt.race_id = src.race_id AND tgt.driver_id = src.driver_id AND tgt.lap = src.lap AND tgt.race_id = src.race_id"
+merge_delta_data(final_df, "f1_processed", "lap_times", processed_folder_path, merge_condition, "race_id")
 
 # COMMAND ----------
 
